c3a1ee5bb7691322a63cbb7e568683089bd558f2
Kapadokia-Titus <ktnyandoch@gmail.com>
2020-07-04 02:25:17 +0300

    added content providers2

--- app/src/main/java/kapadokia/nyandoro/cpdemo/data/NationContract.java
+++ app/src/main/java/kapadokia/nyandoro/cpdemo/data/NationContract.java
@@ -2,12 +2,25 @@ package kapadokia.nyandoro.cpdemo.data;
 
 // contains constants that define names for URIs, table and columns
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public final class NationContract{
 
+    //defining the authority string
+    public static final String CONTENT_AUTHORITY ="kapadokia.nyandoro.cpdemo.data.NationPovider";
+    // declare content Uri
+    // BASE_CONTENT_URI will be content://kapadokia.nyandoro.cpdemo.data.NationPovider
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://"+CONTENT_AUTHORITY);
+    // path to get client app to our table
+    public static final String PATH_COUNTRIES= "countries";
     //define an inner class for our database table;
     public static final class NationEntry implements BaseColumns{
+
+        // declare the final content Uri
+        // CONTENT_URI will be content://kapadokia.nyandoro.cpdemo.data.NationPovider/countries
+         public static final Uri CONTENT_URI =  Uri.withAppendedPath(BASE_CONTENT_URI, PATH_COUNTRIES);
+
 
         // define table name
         public static final String TABLE_NAME= "countries";
---
+++ app/src/main/java/kapadokia/nyandoro/cpdemo/data/NationPovider.java
@@ -0,0 +1,50 @@
+package kapadokia.nyandoro.cpdemo.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class NationPovider extends ContentProvider {
+
+    // declaring the reference of the NationDbHelper class
+    private NationDbHelper databaseHelper;
+    @Override
+    public boolean onCreate() {
+        databaseHelper = new NationDbHelper(getContext());
+
+        // return true means the completion of initialization of the content provider
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+}
